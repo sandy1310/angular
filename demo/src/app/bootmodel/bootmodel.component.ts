923f5b48f2245574e9d3f5198b09075d6515b11b
Sandeep Kumar <sk8602401705@gmail.com>
2021-09-03 10:13:55 +0530

    add bootmoda
    create new componet

---
+++ demo/src/app/bootmodel/bootmodel.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-bootmodel',
+  templateUrl: './bootmodel.component.html',
+  styleUrls: ['./bootmodel.component.css']
+})
+export class BootmodelComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
